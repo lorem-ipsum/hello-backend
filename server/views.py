faf950ce9445fd4a08657b73a367fdf47e01c1d5
CLT <1330525233@qq.com>
2020-06-26 16:10:03 +0800

    basic usage

---
+++ server/views.py
@@ -0,0 +1,23 @@
+import json
+import uuid
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Person
+
+# Create your views here.
+
+
+def write_server(request):
+    print('request.body: ', request.body)
+
+    data = json.loads(request.body)
+    data['id'] = uuid.uuid4()
+    Person.objects.create(**data)
+    return JsonResponse({'success': True})
+
+
+def read_server(request):
+    name = request.GET['name']
+    data = serializers.serialize('python', Person.objects.filter(name=name))
+    return JsonResponse({'success': True, 'data': data})
