54ecab28b678c8c3e198d0113d6ef0eef75b368c
lorem-ipsum <67265790+lorem-ipsum@users.noreply.github.com>
2020-06-27 11:26:40 +0800

    Update settings.py

--- service/settings.py
+++ service/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
